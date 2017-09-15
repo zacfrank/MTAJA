a057f4f26363b1f550ba2a15f0e73816ab1b6497
matthewhudson <matthud@gmail.com>
2017-09-15 17:22:11 -0400

    Cleaning up initial implementation.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# President Who?
+# MTAJA
 
 President 45 is very temperamental, he deserves a name to match.
--- content_script.js
+++ content_script.js
@@ -23,15 +23,51 @@ function walk(node) {
   }
 }
 
-const MR_PRESIDENT = 'Short Fingered Vulgarian';
-
 function handleText(textNode) {
+  var names = [
+    'the short-fingered vulgarian',
+    'Comrade Trump',
+    'the American experiment at the diminutive paws',
+    'an aggrieved social-media manager',
+    'a beleaguered L.P.G.A. enthusiast',
+    "the Wendy's aficionado",
+    'poor woebegone D.J. Trump',
+    'our amateur crisis communications manager',
+    'the aggrieved Vladimir Putin frenemy',
+    'the Boy Scout lover',
+    'the forsaken Anthony Scaramucci supporter',
+    'the vowed ice cream lover',
+    'the amateur policing expert',
+    'the forsaken Jeff Sessions comrade',
+    "the forlorn ladies' golf aficionado and jilted Jeff Sessions comrade",
+    'the "master fracker" who thinks health insurance costs $12 per year',
+    'the voracious ice-cream eater',
+    'the aggrieved primogeniture victim',
+    'the aggrieved food-and-beverage proprietor'
+  ];
+
+  var randomNameIndex = Math.floor(Math.random() * names.length);
+  var replacementName = names[randomNameIndex];
   var text = textNode.nodeValue;
 
-  text = text.replace(/TRUMP/gi, MR_PRESIDENT);
-  text = text.replace(/Trump/gi, MR_PRESIDENT);
-  text = text.replace(/trump/gi, MR_PRESIDENT);
-  text = text.replace(/\btrump\b/gi, MR_PRESIDENT);
+  text = text.replace(/President\sDonald\sTrump/gi, replacementName);
+  text = text.replace(/President\sDonald\sJ\.\sTrump/gi, replacementName);
+  text = text.replace(/Donald\sJ\.\sTrump/gi, replacementName);
+  text = text.replace(/Donald\sTrump/gi, replacementName);
+  text = text.replace(/President\sTrump/gi, replacementName);
+  text = text.replace(
+    /the\spresident\sof\sthe\sUnited\sStates/gi,
+    replacementName
+  );
+  text = text.replace(
+    /the\spresident\sof\sthe\sUnited\sStates\sof\sAmerica/gi,
+    replacementName
+  );
+  text = text.replace(/the\spresident\sof\sthe\sU\.S\./gi, replacementName);
+  text = text.replace(/the\spresident\sof\sthe\sU\.S\.A\./gi, replacementName);
+  text = text.replace(/the\sAmerican\spresident/gi, replacementName);
+  text = text.replace(/the\sU\.S\.\spresident/gi, replacementName);
+  text = text.replace(/Mr\.\sTrump/gi, replacementName);
 
   textNode.nodeValue = text;
 }
